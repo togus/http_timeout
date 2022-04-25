c9078e4fdef2fcf65ffa54e9c0616d6f0c9d64ab
Tobias Gustavsson <tobias@itopi.se>
2022-04-25 21:33:01 +0200

    Simple wait x seconds for response is working

--- http.go
+++ http.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+func startHTTPServer(port, timeout int) {
+	http.HandleFunc("/test", getTimeoutHandler(timeout))
+	log.Println("Starting http server on port", port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+}
+
+func getTimeoutHandler(timeout int) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Request", r.URL, r.Host, r.Method)
+		ctx := r.Context()
+		keys, ok := r.URL.Query()["time"]
+		var t = timeout // default timeout
+		if ok {
+			t, _ = strconv.Atoi(keys[0])
+		}
+		select {
+		case <-time.After(time.Duration(t) * time.Second):
+			fmt.Fprintf(w, "Waited %d seconds\n", t)
+		case <-ctx.Done():
+			fmt.Fprintf(w, "Error, timeout not reached")
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
-import "flag"
+import (
+	"flag"
+)
 
 var port int
+var timeout int
 
 func main() {
-	flag.IntVar(&port, "post", 8080, "Listening http port")
-
+	flag.IntVar(&port, "port", 8080, "Listening http port")
+	flag.IntVar(&timeout, "timeout", 2, "Default timeout in seconds")
 	flag.Parse()
 
+	startHTTPServer(port, timeout)
 }
